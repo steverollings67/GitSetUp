4d89ccab3b0ca3150311f7e362c4f7abc5f3542b
P2698931 <P2698931@SRNCSEGDL3358JN>
2021-07-29 19:36:30 -0400

    Add project files.

---
+++ ClassPractice/Dogs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    public class Dogs
+    {
+        public int Age;
+        public string Name;
+        public string Breed;
+        public bool HasOwner;
+
+        public void Greeting()
+        {
+            Console.WriteLine("My dog's name is " + Name + " so don't where it out.\nMy dog's age is " + Age + ".");
+            Console.WriteLine("The breed of my dog is " + Breed + ".");
+        }
+    }
+}
---
+++ ClassPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dogs dog = new Dogs();
+
+            dog.Age = 3;
+            dog.Name = "Spot";
+            dog.Breed = "Lab";
+            dog.Greeting();
+
+            Dogs dog2 = new Dogs();
+            dog2.Age = 4;
+            dog2.Name = "Cindy";
+            dog2.Breed = "Chihuahua";
+            dog2.Greeting();
+
+
+
+        }
+    }
+}
